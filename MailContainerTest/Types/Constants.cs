8c486fa10eadfd0f98568b5d543db2b4fe2ca006
Oludayo Alli <sunnymexy@gmail.com>
2023-06-10 08:02:31 +0100

    Mail Helper

---
+++ MailContainerTest/Types/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailContainerTest.Types
+{
+    public static class Constants
+    {
+        public const string DATASTORE_NAME = "DataStoreType";
+        public const string BACKUP_DATASTORE_TYPE = "Backup";
+    }
+}
---
+++ MailContainerTest/Utils/MailHelper.cs
@@ -0,0 +1,28 @@
+﻿using MailContainerTest.Data;
+using MailContainerTest.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailContainerTest.Utils
+{
+    public static class MailHelper
+    {
+        public static MailContainer GetMailFromContainter(string dataStoreType, string containerNumber)
+        {
+            if (dataStoreType == "Backup")
+            {
+                var mailContainerDataStore = new BackupMailContainerDataStore();
+                return mailContainerDataStore.GetMailContainer(containerNumber);
+
+            }
+            else
+            {
+                var mailContainerDataStore = new MailContainerDataStore();
+                return mailContainerDataStore.GetMailContainer(containerNumber);
+            }
+        }
+    }
+}
