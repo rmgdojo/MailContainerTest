1ba26d24f17cf8e9142f1750b0a8208633275394
Julian L <julian.lawrance@gmail.com>
2024-01-10 14:51:55 +0000

    Add unit tests to verify a container's allowed mail type is respected when transferring mail to it

---
+++ MailContainerTest.Tests/MailTransferServiceTests.cs
@@ -0,0 +1,78 @@
+using MailContainerTest.Data;
+using MailContainerTest.Services;
+using MailContainerTest.Types;
+using Moq;
+using NUnit.Framework;
+
+namespace MailContainerTest.Tests
+{
+    public class MailTransferServiceTests
+    {
+        [Test]
+        [TestCase(MailType.StandardLetter, AllowedMailType.StandardLetter)]
+        [TestCase(MailType.StandardLetter, AllowedMailType.StandardLetter | AllowedMailType.LargeLetter)]
+        [TestCase(MailType.StandardLetter, AllowedMailType.StandardLetter | AllowedMailType.SmallParcel)]
+        [TestCase(MailType.StandardLetter, AllowedMailType.StandardLetter | AllowedMailType.LargeLetter | AllowedMailType.SmallParcel)]
+        [TestCase(MailType.LargeLetter, AllowedMailType.LargeLetter)]
+        [TestCase(MailType.LargeLetter, AllowedMailType.StandardLetter | AllowedMailType.LargeLetter)]
+        [TestCase(MailType.LargeLetter, AllowedMailType.LargeLetter | AllowedMailType.SmallParcel)]
+        [TestCase(MailType.LargeLetter, AllowedMailType.StandardLetter | AllowedMailType.LargeLetter | AllowedMailType.SmallParcel)]
+        [TestCase(MailType.SmallParcel, AllowedMailType.SmallParcel)]
+        [TestCase(MailType.SmallParcel, AllowedMailType.StandardLetter | AllowedMailType.SmallParcel)]
+        [TestCase(MailType.SmallParcel, AllowedMailType.LargeLetter | AllowedMailType.SmallParcel)]
+        [TestCase(MailType.SmallParcel, AllowedMailType.StandardLetter | AllowedMailType.LargeLetter | AllowedMailType.SmallParcel)]
+        public void MakeMailTransfer_MailTypeMatch_AllowsTransfer(MailType mailType, AllowedMailType allowedMailType)
+        {
+            var dataStore = new Mock<IMailContainerDataStore>();
+            dataStore.Setup(m => m.GetMailContainer(It.IsAny<string>())).Returns(new MailContainer
+            {
+                AllowedMailType = allowedMailType,
+                Capacity = 10,
+            });
+
+            var transferService = new MailTransferService(dataStore.Object);
+
+            var request = new MakeMailTransferRequest
+            {
+                MailType = mailType,
+                NumberOfMailItems = 1,
+            };
+
+            var result = transferService.MakeMailTransfer(request);
+
+            Assert.That(result.Success, Is.True);
+        }
+
+        [Test]
+        [TestCase(MailType.StandardLetter, AllowedMailType.LargeLetter)]
+        [TestCase(MailType.StandardLetter, AllowedMailType.SmallParcel)]
+        [TestCase(MailType.StandardLetter, AllowedMailType.LargeLetter | AllowedMailType.SmallParcel)]
+        [TestCase(MailType.LargeLetter, AllowedMailType.StandardLetter)]
+        [TestCase(MailType.LargeLetter, AllowedMailType.SmallParcel)]
+        [TestCase(MailType.LargeLetter, AllowedMailType.StandardLetter | AllowedMailType.SmallParcel)]
+        [TestCase(MailType.SmallParcel, AllowedMailType.StandardLetter)]
+        [TestCase(MailType.SmallParcel, AllowedMailType.LargeLetter)]
+        [TestCase(MailType.SmallParcel, AllowedMailType.StandardLetter | AllowedMailType.LargeLetter)]
+        public void MakeMailTransfer_MailTypeMismatch_PreventsTransfer(MailType mailType, AllowedMailType allowedMailType)
+        {
+            var dataStore = new Mock<IMailContainerDataStore>();
+            dataStore.Setup(m => m.GetMailContainer(It.IsAny<string>())).Returns(new MailContainer
+            {
+                AllowedMailType = allowedMailType,
+                Capacity = 10,
+            });
+
+            var transferService = new MailTransferService(dataStore.Object);
+
+            var request = new MakeMailTransferRequest
+            {
+                MailType = mailType,
+                NumberOfMailItems = 1,
+            };
+
+            var result = transferService.MakeMailTransfer(request);
+
+            Assert.That(result.Success, Is.False);
+        }
+    }
+}
