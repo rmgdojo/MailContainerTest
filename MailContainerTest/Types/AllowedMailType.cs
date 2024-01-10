0dfe8903abbe0a8fa0ba9c18d181f4e6175cd46f
Julian L <julian.lawrance@gmail.com>
2024-01-10 15:42:18 +0000

    Remove unnecessary change in AllowedMailType enum

--- MailContainerTest/Types/AllowedMailType.cs
+++ MailContainerTest/Types/AllowedMailType.cs
@@ -1,10 +1,9 @@
 ﻿namespace MailContainerTest.Types
 {
-    [Flags]
     public enum AllowedMailType
     {
         StandardLetter = 1,
         LargeLetter = 2,
-        SmallParcel = 4
+        SmallParcel = 3
     }
 }
