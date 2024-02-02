d0cc307b7049a2c95ac61c53009b7cf7885b25e5
lolade oyebanji <loladeking@hotmail.com>
2024-02-02 23:03:02 +0000

    added some changes to the tests to make it work

--- MailContainerTest.Tests/MailTransferService_Tests.cs
+++ MailContainerTest.Tests/MailTransferService_Tests.cs
@@ -3,14 +3,10 @@ using MailContainerTest.Services;
 using MailContainerTest.Types;
 using Moq;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MailContainerTest.Tests
 {
+    [TestFixture]
     public class MailTransferService_Tests
     {
         private Mock<IMailContainerDataStoreFactory> _mailContainerDataStoreFactory = default!;
