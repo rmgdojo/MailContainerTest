eead40dd186fbadfa4cb1c4921c6c47bc7a73c18
lolade oyebanji <loladeking@hotmail.com>
2024-02-02 22:08:55 +0000

    added setup and initialization in unit test

--- MailContainerTest.Tests/MailTransferService_Tests.cs
+++ MailContainerTest.Tests/MailTransferService_Tests.cs
@@ -1,5 +1,6 @@
 ﻿using MailContainerTest.Services;
 using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +14,24 @@ namespace MailContainerTest.Tests
         private Mock<IMailContainerDataStoreFactory> _mailContainerDataStoreFactory = default!;
         private Mock<IMailTypeChecker> _mailTypeChecker = default!;
         private Mock<IMailContainerDataStore> _mailContainerDataStore = default!;
-        //private Mock<IConfigurationManager> _configurationManager = default!;
+        private Mock<IConfigurationManager> _configurationManager = default!;
         private Mock<ICheckMailContainerStatus> _checkMailContainer = default!;
         private MailTransferService? _subjectMailTransferService;
         private MailContainerDataStoreFactory? _subjectMailContainerDataStoreFactory;
         private CheckMailContainerStatus _subjectCheckMailContainerStatus = default!;
         private MailTypeChecker _subjectMailTypeChecker = default!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mailContainerDataStoreFactory = new Mock<IMailContainerDataStoreFactory>();
+            _mailTypeChecker = new Mock<IMailTypeChecker>();
+            _configurationManager = new Mock<IConfigurationManager>();
+            _mailContainerDataStore = new Mock<IMailContainerDataStore>();
+            _checkMailContainer = new Mock<ICheckMailContainerStatus>();
+
+            _subjectCheckMailContainerStatus = new CheckMailContainerStatus();
+            _subjectMailTypeChecker = new MailTypeChecker();
+        }
     }
 }
