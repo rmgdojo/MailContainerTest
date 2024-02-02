773d64787e181f219f9a7f19610e91f23f3252da
lolade oyebanji <loladeking@hotmail.com>
2024-02-02 22:16:13 +0000

    Added another unit tests

--- MailContainerTest.Tests/MailTransferService_Tests.cs
+++ MailContainerTest.Tests/MailTransferService_Tests.cs
@@ -1,4 +1,6 @@
-﻿using MailContainerTest.Services;
+﻿using MailContainerTest.Data;
+using MailContainerTest.Services;
+using MailContainerTest.Types;
 using Moq;
 using NUnit.Framework;
 using System;
@@ -32,6 +34,50 @@ namespace MailContainerTest.Tests
 
             _subjectCheckMailContainerStatus = new CheckMailContainerStatus();
             _subjectMailTypeChecker = new MailTypeChecker();
+        }
+
+        [Test]
+        public void MailContainerDataStoreFactory_WhenDataStoreTypeIsbackup_ReturnBackUpMailDataStore()
+        {
+            _subjectMailContainerDataStoreFactory = new MailContainerDataStoreFactory();
+            var factory = _subjectMailContainerDataStoreFactory.CreateMailContainerDataStore("backup");
+            Assert.That(factory, Is.InstanceOf<BackupMailContainerDataStore>());
+        }
+
+        [Test]
+        public void MailContainerDataStoreFactory_WhenDataStoreTypeIsNotBackup_ReturnMailContainerDataStore()
+        {
+            _subjectMailContainerDataStoreFactory = new MailContainerDataStoreFactory();
+
+            var factory = _subjectMailContainerDataStoreFactory.CreateMailContainerDataStore("main");
+
+            Assert.That(factory, Is.InstanceOf<MailContainerDataStore>());
+        }
+
+        [TestCase("backup", true, ExpectedResult = true)]
+        [TestCase("whatever", false, ExpectedResult = false)]
+        public bool? MakeMailTransfer_WithBackupDataStoreType_EnsureSuccess(string dataStoreType, bool resultStatus)
+        {
+            //arrange
+
+            _configurationManager.Setup(x => x.AppSettings(It.IsAny<string>())).Returns(dataStoreType);
+
+            _mailContainerDataStoreFactory.Setup(x => x.CreateMailContainerDataStore(It.IsAny<string>())).Returns(new BackupMailContainerDataStore());
+
+            _mailContainerDataStore.Setup(x => x.GetMailContainer(It.IsAny<string>())).Returns(new MailContainer());
+
+
+            _checkMailContainer.Setup(c => c.CheckContainerStatus(It.IsAny<MailContainer>())).Returns(new MakeMailTransferResult { Success = resultStatus });
+
+            _mailTypeChecker.Setup(x => x.CheckMail(MailType.StandardLetter, It.IsAny<MailContainer>())).Returns(new MakeMailTransferResult { Success = resultStatus });
+
+            _subjectMailTransferService = new MailTransferService(_mailContainerDataStoreFactory.Object,  _checkMailContainer.Object, _mailTypeChecker.Object, _configurationManager.Object);
+
+            //act
+            var result = _subjectMailTransferService?.MakeMailTransfer(new Types.MakeMailTransferRequest());
+
+            //assert
+            return result?.Success;
         }
     }
 }
